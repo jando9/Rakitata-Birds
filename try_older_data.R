79004b843f27ecc7c16195c6d443f34c866cc1a5
Jack Anderson <118315628+jando9@users.noreply.github.com>
2025-06-04 15:24:44 +1200

    try to split sections by hectare proportions

--- try_older_data.R
+++ try_older_data.R
@@ -2,11 +2,12 @@ library(lme4)
 library(MASS) 
 library(ggplot2)
 
-source("initial_look.R")
+source("read_organise_data.R")
 
 # set up data ------------------------------------------------------------------
-subset_of_interest <- list(species = c("Wrybill","Black-fronted tern"), start_year = 0, end_year = 2025)
-specified_years <- subset(UR_data, Year > subset_of_interest$start_year - 1 &
+subset_of_interest <- list(species = c("Wrybill","Black-fronted tern", "Banded dotterel"),
+                           start_year = 1980, end_year = 2025)
+specified_years <- subset(filled_counts_noDups, Year > subset_of_interest$start_year - 1 &
                             Year < subset_of_interest$end_year + 1)
 
 for (i in 1:length(subset_of_interest$species)) {
@@ -14,9 +15,9 @@ for (i in 1:length(subset_of_interest$species)) {
     group_by(section_number, Year, Hectares) %>% summarise(Number = sum(Number))
   subset_species$section_number <- factor(subset_species$section_number)
   subset_species$centeredYear <- subset_species$Year - min(subset_species$Year)
-  subset_species$logHectare <- log(subset_species$Hectares)
   subset_species$factorYear <- as.factor(subset_species$centeredYear)
-  assign(paste0(gsub(" |-", "_", subset_of_interest$species[i]),"_old_dat"), subset_species)
+  with_flow <- inner_join(subset_species, flow_and_observers, by = "Year")
+  assign(paste0(gsub(" |-", "_", subset_of_interest$species[i])), with_flow)
 }
 
 ggplot(data = Wrybill,
@@ -24,7 +25,41 @@ ggplot(data = Wrybill,
   geom_line() +
   geom_point()
 
-Wrybill_old_data <- glmer.nb(Number ~ centeredYear + (1 | section_number), offset = logHectare,
+## Try splitting categories by proportion of area
+after2021_sections <- subset(section_key, year(Date) > 2021)
+full_area <- sum(unique(after2021_sections$Hectares))
+sections_to_fix <- c("Part of 1", "Part of  1, part of  2", "2,3,4", "1,2", "2,3", "Part of  1, 2")
+
+part1 <- 1
+part1_part2 <- unique(c(one=section_key$Hectares[section_key$section_number=="1"]/
+                  section_key$Hectares[section_key$section_number=="Part of  1, part of  2"],
+                  two=section_key$Hectares[section_key$section_number=="2"]/
+                  section_key$Hectares[section_key$section_number=="Part of  1, part of  2"]))
+sect_234 <- unique(c(two=section_key$Hectares[section_key$section_number=="2"]/
+              section_key$Hectares[section_key$section_number=="2,3,4"],
+              three=section_key$Hectares[section_key$section_number=="3"]/
+              section_key$Hectares[section_key$section_number=="2,3,4"],
+              four=section_key$Hectares[section_key$section_number=="4"]/
+              section_key$Hectares[section_key$section_number=="2,3,4"]))
+sect_12 <- unique(c(one=section_key$Hectares[section_key$section_number=="1"]/
+             section_key$Hectares[section_key$section_number=="1,2"],
+             two=section_key$Hectares[section_key$section_number=="2"]/
+             section_key$Hectares[section_key$section_number=="1,2"]))
+sect_23 <- unique(c(two=section_key$Hectares[section_key$section_number=="2"]/
+                    unique(section_key$Hectares[section_key$section_number=="2,3"]),
+                    three=section_key$Hectares[section_key$section_number=="3"]/
+                    unique(section_key$Hectares[section_key$section_number=="2,3"])))
+part1_2 <- unique(c(one=section_key$Hectares[section_key$section_number=="1"]/
+                      section_key$Hectares[section_key$section_number=="Part of  1, 2"],
+                    two=section_key$Hectares[section_key$section_number=="2"]/
+                      section_key$Hectares[section_key$section_number=="Part of  1, 2"]))
+
+# need to split sections up and multiply by these proportions above
+                                                 
+
+
+
+Wrybill_old_data <- glmer.nb(Number ~ centeredYear + (1 | section_number), offset = log(mean_daily_surveyors),
                      data = Wrybill_old_dat)
 summary(Wrybill_old_data)
 resid_df <- 37
